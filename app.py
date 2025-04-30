cb593d9776f8214942b4f16cfd35a0c2d2d09845
Amir FARES <48154837+AmirFARES@users.noreply.github.com>
2025-04-30 15:04:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title("Looker Studio Dashboard")
 
 # Embed Looker Studio report using raw HTML
 looker_embed_html = """
-<iframe width="600" height="450" 
+<iframe width="1400" height="1000" 
 src="https://lookerstudio.google.com/embed/reporting/e1d9de8a-5c36-4581-8c8b-1060893902f9/page/h0zdE" 
 frameborder="0" style="border:0" allowfullscreen 
 sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox">
